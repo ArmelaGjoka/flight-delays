c0d50ff68eb651fd292dee9a59ba0d8cd3d61058
Armela Gjoka <armela@matrics.com>
2021-10-02 11:01:36 +0200

    init frontend app

---
+++ apps/flights-delay/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'flights-delay',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/flights-delay',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
