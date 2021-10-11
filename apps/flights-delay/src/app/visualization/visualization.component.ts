9edff3a3be5a99e93aabf3fda62b4a6189329a62
Armela Gjoka <armela@matrics.com>
2021-10-11 07:18:10 +0200

    Update data

--- apps/flights-delay/src/app/visualization/visualization.component.ts
+++ apps/flights-delay/src/app/visualization/visualization.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { View } from 'vega';
 
@@ -19,23 +19,55 @@ export class VisualizationComponent implements OnInit {
   toAirport = new FormControl();
 
   // Setup graph filters datasource
-  fromAirportList: string[] = ['A1', 'A2', 'A3'];
-  toAirportList: string[] = ['A1', 'A2', 'A3'];
+  fromAirportList: string[] = [];
+  toAirportList: string[] = [];
 
-  @Input() 
-    set data(values: any[]){
-       if (!this.view) {
-           return;
-       }
-        this.view.remove('table', this.view.data('table')).run();
-        this.view.insert('table', values).run();
-    }
+  set airports(values: any[]){
+      if (!this.view) {
+          return;
+      }
+      this.view.remove('airports', this.view.data('table')).run();
+      this.view.insert('airports', values).run();
+  }
 
+
+
+
+  
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.http.get('../assets/map-visualization.json').subscribe(spec => this.vegaInit(spec));
+    this.http.get('../assets/data/flights-airport.csv',  {responseType: "text"}).subscribe(data => {
+      const list = data.split("\n");
+      const fromSet = new Set<string>();
+      const toSet = new Set<string>();
+      list.forEach( flight => {
+            const values = flight.split(",");
+            fromSet.add(values[0]);
+            toSet.add(values[1]);
+        });
+      this.fromAirportList = Array.from(fromSet.values());
+      this.toAirportList = Array.from(toSet.values());
+    });
+
+    this.fromAirport.valueChanges.subscribe(newValue => {
+       console.log('New Value: ', this.view?.data('airports'), this.view?.getState());
+       const currentState  = this.view?.getState().signals;
+
+       const airports = this.view?.data('airports');
+
+       const selectedAirport = airports?.filter(a => a.iata = newValue)[0];
+       
+       const newState = {...currentState, hover: selectedAirport };
+
+       this.view?.setState({
+         signals: newState
+       });
+       this.view?.runAsync();
+       console.log('After: ', newValue, this.view?.getState());
+    })
   }
 
   public vegaInit(spec: any): any {
