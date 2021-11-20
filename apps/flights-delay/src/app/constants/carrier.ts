be22d409644720ba7ec0f38d167527e0aab5e1c3
Armela Gjoka <armela@matrics.com>
2021-11-20 19:24:03 +0100

    Update predict form

---
+++ apps/flights-delay/src/app/constants/carrier.ts
@@ -0,0 +1,13 @@
+
+export const CARRIER_LIST = [
+'Southwest Airlines Co.',
+'American Airlines Inc.',
+'United Air Lines Inc.',
+'Hawaiian Airlines Inc.',
+'Alaska Airlines Inc.',
+'Frontier Airlines Inc.',
+'Spirit Air Lines',
+'Delta Air Lines Inc.',
+'Allegiant Air',
+'JetBlue Airways'
+];
--- apps/flights-delay/src/app/view-container/components/form/form.component.ts
+++ apps/flights-delay/src/app/view-container/components/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { CARRIER_LIST } from '../../../constants/carrier';
 
 @Component({
   selector: 'flight-delays-form',
@@ -7,12 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  carriers = CARRIER_LIST;
+
+  form = new FormGroup({
+    // Flight
+    departureDelay: new FormControl(), // Avg default
+    distance: new FormControl(), // Current Distance
+    // Time
+    hour: new FormControl(),
+    day: new FormControl(),
+    month: new FormControl(),
+    // Covid
+    covidOrigin: new FormControl(),
+    covidDest: new FormControl(),
+    increaseOrigin: new FormControl(),
+    increaceDest: new FormControl(),
+    // Airline
+    airline: new FormControl(),
+  })
+
   constructor() { 
     console.log('TODO FORM');
   }
 
   ngOnInit(): void {
     console.log('TODO FORM');
+  }
+
+  predictDelay() {
+    console.log('Predict delay');
   }
 
 }
